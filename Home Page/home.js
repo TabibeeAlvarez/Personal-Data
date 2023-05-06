b7b273565a29dbb2367eec393f9e975ba59e9523
eXclusive Tshirt <116873249+TabibeeAlvarez@users.noreply.github.com>
2023-05-07 03:37:19 +0800

    Add files via upload

--- Home Page/home.js
+++ Home Page/home.js
@@ -2,6 +2,7 @@ function labas(){
     document.getElementById("show").style.display = 'block';
     document.getElementById("show2").style.display = 'block';
     document.getElementById("show3").style.display = 'block';
+    document.getElementById("show4").style.display = 'block';
     document.getElementById("x").style.display = 'block'
     document.getElementById("menu").style.display = 'none'
 }
@@ -10,6 +11,11 @@ function tago(){
     document.getElementById("show").style.display = 'none';
     document.getElementById("show2").style.display = 'none';
     document.getElementById("show3").style.display = 'none';
+    document.getElementById("show4").style.display = 'none';
     document.getElementById("x").style.display = 'none'
     document.getElementById("menu").style.display = 'block'
+}
+
+function back(){
+    window.location.href = 'https://tabibeealvarez.github.io/Personal-Data/';
 }
